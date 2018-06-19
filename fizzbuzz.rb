4ff1718f965484eb9e97adc7ff18c730db6f104f
umxe <u_habiba@rocketmail.com>
2018-06-19 15:23:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,8 @@ def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
   end
-  else if int % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  end
   if int % 5 == 0 && int % 3 == 0
     puts "FizzBuzz"
   end
