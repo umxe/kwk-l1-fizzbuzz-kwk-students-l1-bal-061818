cde28bdbcc00524ed8fc91ebc8a43578358b7362
umxe <u_habiba@rocketmail.com>
2018-06-19 15:25:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if (int % 5 == 0) && (int % 3 == 0)
     puts "Fizz"
   elsif int % 5 == 0
     puts "Buzz"
-  elsif (int % 5 == 0) && (int % 3 == 0)
+  elsif int % 3 == 0
     puts "FizzBuzz"
   else
     puts "The number is not divisible by 3 or 5."
